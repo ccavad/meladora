956518898d476b8cd3838a29250921b748dd21d1
ccavad <ccavad2015@gmail.com>
2022-09-11 16:08:55 +0400

    second commit

--- js/script-m-1.js
+++ js/script-m-1.js
@@ -1,112 +1,111 @@
-// dom elements 
+
+
+// dom elements
 const productsTitles = document.querySelectorAll(".products li");
 const gridItems = document.querySelectorAll(".products .grid-item");
 const searchBoxBtn = document.querySelector(".searchbox-btn");
 const searchWindow = document.querySelector(".search-window");
-const closeBtn = document.querySelector(".close-btn"); 
+const closeBtn = document.querySelector(".close-btn");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const pagesNav = document.querySelector(".pages-nav");
 const mobileOverlay = document.querySelector(".mobile-overlay");
 const closeMobileNav = document.querySelector(".closeMobileNav");
 
-// event listeners 
+// event listeners
 
 function watchBreakpoint(x) {
-  if (x.matches) { 
+  if (x.matches) {
     pagesNav.style.display = "flex";
-    mobileOverlay.style.display = "none"
+    mobileOverlay.style.display = "none";
   } else {
-    pagesNav.style.display = "none"
+    pagesNav.style.display = "none";
   }
 }
 
-let breakpoint = window.matchMedia("(min-width: 992px)")
-watchBreakpoint(breakpoint) // Call listener function at run time
-breakpoint.addListener(watchBreakpoint) // Attach listener function on state changes
+let breakpoint = window.matchMedia("(min-width: 992px)");
+watchBreakpoint(breakpoint); // Call listener function at run time
+breakpoint.addListener(watchBreakpoint); // Attach listener function on state changes
 
 hamburgerBtn.addEventListener("click", () => {
   pagesNav.style.display = "flex";
-  mobileOverlay.style.display = "block"
-})
+  mobileOverlay.style.display = "block";
+});
 
 closeMobileNav.addEventListener("click", () => {
   pagesNav.style.display = "none";
-  mobileOverlay.style.display = "none"
-})
+  mobileOverlay.style.display = "none";
+});
 
 mobileOverlay.addEventListener("click", () => {
   pagesNav.style.display = "none";
-  mobileOverlay.style.display = "none"
-})
-
+  mobileOverlay.style.display = "none";
+});
 
 searchBoxBtn.addEventListener("click", () => {
   searchWindow.style.top = "0";
-})
+});
 
 closeBtn.addEventListener("click", () => {
-  searchWindow.style.top = "-100%"
-})
+  searchWindow.style.top = "-100%";
+});
 
-// products filter 
-productsTitles.forEach(title => {
+// products filter
+productsTitles.forEach((title) => {
   title.addEventListener("click", (e) => {
-    const tag = e.target.dataset.tag
-    console.log(tag)
-    productsTitles.forEach(title => {
-      title.classList.remove("active")
-    })
-    title.classList.add("active")
+    const tag = e.target.dataset.tag;
+    productsTitles.forEach((title) => {
+      title.classList.remove("active");
+    });
+    title.classList.add("active");
 
-    gridItems.forEach(item => item.style.display = "block")
-    gridItems.forEach(item => {
+    gridItems.forEach((item) => (item.style.display = "block"));
+    gridItems.forEach((item) => {
       if (item.dataset.tag !== tag) {
-        item.style.display = "none"
+        item.style.display = "none";
       }
-    })
+    });
 
     if (tag === "hamisi") {
-      gridItems.forEach(item => item.style.display = "block")
+      gridItems.forEach((item) => (item.style.display = "block"));
     }
-  })
+  });
+});
+
+// window scroll
+
+window.onscroll = function () {
+  scrollFunction();
+  console.log("window scrolled")
+};
+
+window.addEventListener("scroll", () => {
+  console.log("scrolled")
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.querySelector(".header-bottom").classList.add("fixed");
+  } else {
+    document.querySelector(".header-bottom").classList.remove("fixed");
+  }
 })
 
-// window scroll 
-
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.querySelector(".header-bottom").classList.add("fixed")
-    } else {
-      document.querySelector(".header-bottom").classList.remove("fixed")
-    }
+  if (document.body.scrollTop > 0) {
+    document.querySelector(".header-bottom").classList.add("fixed");
+  } else {
+    document.querySelector(".header-bottom").classList.remove("fixed");
+  }
 }
 
-// slider 
+// slider
 
 let swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   spaceBetween: 50,
-  // keyboardControl: true,
-  // allowTouchMove: true,
-  // lazy: true,
-  // loop: true,
   pagination: {
     el: ".swiper-pagination",
-    // dynamicBullets: true,
     paginationClickable: true,
     clickable: true,
-  }, 
-  // breakpoints: {
-  //   514: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 20,
-  //     allowTouchMove: true
-  //   },
-  //   756: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20
-  //   }
-  // }
+  },
 });
+
+// lightbox
+
